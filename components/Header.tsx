dd6319f68ada0d1e00dc7215268161ca6c8ddcb2
Nisarg Dave <ndave630@gmail.com>
2023-12-25 12:19:44 +0800

    feat: created layout and additional skeleton components for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,24 @@
+"use client";
 import ThemeToggler from "./ThemeToggler";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 function Header() {
   return (
     <header>
-      <ThemeToggler />
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div>
+            <Nav />
+            <ThemeToggler />
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
