40766355e76be8031867993bc17006d82fe51677
Nisarg Dave <ndave630@gmail.com>
2023-12-25 15:36:30 +0800

    feat: added shadow effect to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,12 +3,31 @@ import ThemeToggler from "./ThemeToggler";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    };
+
+    window.addEventListener("scroll", scrollYPos);
+    return () => window.removeEventListener("scroll", scrollYPos);
+  });
   return (
-    <header>
+    <header
+      className={` ${
+        header
+          ? "py-4 bg-white shadow-lg dark:bg-accent"
+          : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all`}
+    >
       <div className="container mx-auto">
-        <div className="flex justify-between items-center mt-2">
+        <div className="flex justify-between items-center">
           <Logo />
           <div className="flex">
             <Nav
