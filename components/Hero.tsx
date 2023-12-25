3e63a141d98993093e1261a475df4923d008a993
Nisarg Dave <ndave630@gmail.com>
2023-12-25 17:21:40 +0800

    feat: started developing the hero component and modified css of button component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,11 +14,31 @@ import Socials from "./Socials";
 
 function Hero() {
   return (
-    <section className="py-12 xl:py-23 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom dark:bg-none">
+    <section className="py-12 xl:py-23 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-cover bg-bottom dark:bg-none">
       <div className="container mx-auto">
         <div className="flex justify-between gap-x-8">
-          <div>text</div>
-          <div>image</div>
+          <div className="flex max-w-[600px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
+              Software Developer
+            </div>
+            <h1 className="h1">Hello, my name is Nisarg Dave</h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              Brief description with insights into myself, my vocational
+              journey, and what I engage in professionally.
+            </p>
+            {/* Buttons */}
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2">
+                  Contact <Send size={18} />
+                </Button>
+              </Link>
+              <Button variant="secondary" className="gap-x-2">
+                Download CV <Download size={18} />
+              </Button>
+            </div>
+          </div>
+          <div className="hidden xl:flex relative">image</div>
         </div>
         <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
           <RiArrowDownSLine className="text-3xl text-primary" />
