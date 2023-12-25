f6accb36b8122a4d1cf31f0ecbfef5234392a77a
Nisarg Dave <ndave630@gmail.com>
2023-12-25 16:12:19 +0800

    feat: added hero background to tailwind config

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,8 +8,24 @@ import {
   RiArrowDownSLine,
 } from "react-icons/ri";
 
+import DevImg from "./DevImg";
+import Badge from "./Badge";
+import Socials from "./Socials";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="py-12 xl:py-23 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom dark:bg-none">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-x-8">
+          <div>text</div>
+          <div>image</div>
+        </div>
+        <div className="hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce">
+          <RiArrowDownSLine className="text-3xl text-primary" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
