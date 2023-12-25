1f4b666939fc7b2e80c100529b8934b350a884a0
Nisarg Dave <50187169+nisarg-dave@users.noreply.github.com>
2023-12-25 15:39:21 +0800

    Feature/header (#3)
    * feat: created layout and additional skeleton components for header
    
    * feat: created and styled the nav
    
    * feat: created logo
    
    * feat: added shadow effect to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,47 @@
+"use client";
 import ThemeToggler from "./ThemeToggler";
+import Logo from "./Logo";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 
 function Header() {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    };
+
+    window.addEventListener("scroll", scrollYPos);
+    return () => window.removeEventListener("scroll", scrollYPos);
+  });
   return (
-    <header>
-      <ThemeToggler />
+    <header
+      className={` ${
+        header
+          ? "py-4 bg-white shadow-lg dark:bg-accent"
+          : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all`}
+    >
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <Logo />
+          <div className="flex">
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center mr-6"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
+            <ThemeToggler />
+            <div className="xl:hidden">
+              <MobileNav />
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   );
 }
